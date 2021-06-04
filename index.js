1ba00a26dce2d2ed680466c2fe56b5525802b0a0
Lincoli Xavier <lincolixavier@gmail.com>
2021-06-04 12:21:21 -0300

    welcome package

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const open = require('open');
+
+console.log("Hello! My name is Lincoli 🤓 \n");
+
+console.log("and whitin 3 seconds you'll be redirect to my website 🚀  \n");
+
+setTimeout(() => {
+  open('http://www.lincolixavier.com');
+  console.log("Thank you, and have a good trip!  \n");
+}, 3000);
+
